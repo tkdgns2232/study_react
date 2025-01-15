71334c855b701361c9ca13825e983167f41c3e3e
tkdgns2232 <tkdgns2232@naver.com>
2025-01-15 17:49:42 +0900

    도서정보 문제풀어보기

---
+++ react-study/src/study/App7.jsx
@@ -0,0 +1,32 @@
+/**
+ * 도서정보 등록 및 조회
+ * 
+ * 도서명,저자명,출판사명 입력
+ * bookList 저장
+ * 
+ * table
+ * tbody
+ * tr
+ * td
+ * 구성으로 만들기
+ * 
+ * 컴포넌트 2개로 나누기
+ * 
+ */
+
+import React from 'react';
+
+function App7(props) {
+    return (
+        <div>
+            <select name="" id="">
+                <option value="">도서명</option>
+                <option value="">저자명</option>
+                <option value="">출판사</option>
+                <option value="">카테고리</option>
+            </select>
+        </div>
+    );
+}
+
+export default App7;
