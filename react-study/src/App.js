114e8763911639f12142ad6d8592804af557d551
tkdgns2232 <tkdgns2232@naver.com>
2025-01-20 17:47:57 +0900

    미니 게시판

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,47 +1,23 @@
-<<<<<<< HEAD
 import { Route, Routes } from "react-router-dom";
 import IndexPage from "./pages/IndexPage/IndexPage";
-import WritePage from "./pages/Write/WritePage";
+import WritePage from "./pages/WritePage/WritePage";
 import { Global } from "@emotion/react";
-import {global} from "./styles/global";
+import { global } from "./styles/global";
 import MainLayout from "./components/MainLayout/MainLayout";
 
 function App() {
-
+  
   return (
     <>
       <Global styles={global} />
 
       <MainLayout>
         <Routes>
-          <Route path="/" element={ <IndexPage/>} /> 
-          <Route path="/write" element={<WritePage/>} />
+          <Route path="/" element={ <IndexPage /> } />
+          <Route path="/write" element={ <WritePage /> } />
         </Routes>
-      </MainLayout>   
+      </MainLayout>
     </>
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> c7f48417f82e2c7f411ec83f043402e63c4a9f07
   );
 }
 
--- react-study/src/study/App14.jsx
+++ react-study/src/study/App14.jsx
@@ -1,40 +1,32 @@
 import axios from 'axios';
 import React from 'react';
+
 function App14(props) {
 
-
-    const handleRequestOnClick =async () => {
+    const handleRequestOnClick = async () => {
         let response = null;
         try {
-            response = await axios.get("http://localhost:8080/servlet_study_war/api/user"); //axios로 요청한다 get을 호출하면 리턴이 promise가된다.
+            response = await axios.get("http://localhost:8080/servlet_study_war/api/user");
             console.log(response);
             console.log(response.data.username);
-        } catch (error){
+        } catch(error) {
             console.error(error);
-
         }
     }
 
     const handleBookRequestOnClick = async () => {
         let response = null;
         try {
-            response = await axios.get("http://localhost:8080/servlet_study_war/api/book"); //axios로 요청한다 get을 호출하면 리턴이 promise가된다.
+            response = await axios.get("http://localhost:8080/servlet_study_war/api/book");
             console.log(response);
-            console.log(response.data.bookId);
-        } catch (error){
+        } catch(error) {
             console.error(error);
-
         }
     }
-<<<<<<< HEAD
-    
-    
-=======
 
->>>>>>> c7f48417f82e2c7f411ec83f043402e63c4a9f07
     /**
      * BookRestServlet(/api/book)
-     * get 요청
+     * get요청
      * bookId,
      * bookName,
      * author,
@@ -47,7 +39,6 @@ function App14(props) {
         <div>
             <button onClick={handleRequestOnClick}>요청</button>
             <button onClick={handleBookRequestOnClick}>도서요청</button>
-
         </div>
     );
 }
